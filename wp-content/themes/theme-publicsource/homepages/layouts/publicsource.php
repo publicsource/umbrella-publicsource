c12270f88dc54f47e761b145adc0571db4e04c45
Ben Keith <ben@benlk.com>
2016-12-13 15:17:24 -0500

    Register Home Bottom Featured prominence term

--- wp-content/themes/theme-publicsource/homepages/layouts/publicsource.php
+++ wp-content/themes/theme-publicsource/homepages/layouts/publicsource.php
@@ -11,8 +11,13 @@ class PublicSourceLayout extends Homepage {
 			'prominenceTerms' 	=> array(
 				array(
 					'name' 			=> __( 'Homepage Top Story', 'largo' ),
-					'description' 	=> __( 'If you are using the Newspaper or Carousel optional homepage layout, add this label to a post to make it the top story on the homepage', 'largo' ),
+					'description' 	=> __( 'This is the top story on the homepage.', 'largo' ),
 					'slug' 			=> 'top-story'
+				),
+				array(
+					'name' 			=> __( 'Homepage Bottom Feature', 'largo' ),
+					'description' 	=> __( 'Posts assigned to this prominence term should appear in the Largo Recent Posts widget in the Home Bottom Feature widget area.', 'largo' ),
+					'slug' 			=> 'home-bottom-feature'
 				),
 			),
 			'assets' => array(
